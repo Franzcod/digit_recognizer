c4db1ca12ed0102da9a931f994fa2996d0a39f15
Palacios Francisco <53352272+Franzcod@users.noreply.github.com>
2020-08-27 07:51:16 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:digit_recognizer/screens/draw_screen.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.amber
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.blueAccent
+        )
+      ),
+      
+      home: DrawScreen(),
+    );
+  }
+}
